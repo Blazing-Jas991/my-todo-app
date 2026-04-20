58d94d5a0137d112a3055dcb04631d9fe721912f
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-04-20 10:36:35 -0700

    add dialog box and set minor styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { greeting } from "./template.js";
+import { monday } from "./template.js";
+import { showDialog } from "./mydomMethods.js";
+
+const addButton = document.querySelector('.add-btn');
+addButton.addEventListener('click', showDialog());
 
 console.log(greeting);
+console.log(monday);
--- src/mydomMethods.js
+++ src/mydomMethods.js
@@ -1,16 +1,3 @@
-export const greeting = "Hello, Odinite!";
-
-class Todo {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-}
-
-export const monday = new Todo('Pick Up Ann', 'Pick up Ann from the Airport', 'Thursday', 'Very Important');
-
 export function showDialog() {
     const dialogContainer = document.querySelector('.content-container');
     const dialog = document.createElement('dialog');
